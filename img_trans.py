70537976847bace1421b846cd5a36bebc982729d
IVEN_He <13377529851@qq.com>
2024-05-28 13:41:35 +0800

    解决已知问题

--- img_trans.py
+++ img_trans.py
@@ -28,46 +28,44 @@ class VideoStreaming(object):
     
     def start(self) -> None:
         """开始传输视频流"""
-        print("Client Host Name:", self.host_name)
-        print("Connection from: ", self.client_address)
-        print("Streaming...")
+        # print("Client Host Name:", self.host_name)
+        # print("Connection from: ", self.client_address)
+        # print("Streaming...")
         self.stream = io.BytesIO()							# 创建一个io流，用于存放二进制数据
 
     def send(self, _img) -> None:
         """发送图像数据
         ----
         * _img: 传入的图像数据"""
-        while True:
+        # while True:
+        try:
             try:
-                try:
-                    img_encode = cv2.imencode('.jpg', _img)[1]						# 编码
-                except:
-                    print('没有读取到图像')
-                    continue
-                data_encode = np.array(img_encode)								    # 将编码数据转换成二进制数据
-                self.stream.write(data_encode)										# 将二进制数据存放到io流
-                self.connect.write(struct.pack('<L', self.stream.tell()))			# struct.pack()将数据转换成什么格式    stream.tell()获得目前指针的位置，将数据写入io流后，数据指针跟着后移，
-                                                                                    # 也就是将数据长度转换成'<L'类型（无符号长整型），写入makefile传输文件
-                                                                                    # 它的作用相当于 帧头数据，单独收到这个数据表示开始传输一帧图片数据，因为图片大小确定，这个数也就定下不变
-                self.connect.flush()											    # 刷新，将数据长度发送出去
-                self.stream.seek(0)													# 更新io流，将指针指向0
-                self.connect.write(self.stream.read())								# 指针指向0后，从头开始读数据，然后写到makefile传输文件
-                self.stream.seek(0)													# 更新指针
-                self.stream.truncate()												# 更新io流数据，删除指针后面的数据
+                img_encode = cv2.imencode('.jpg', _img)[1]						# 编码
+            except:
+                # print('没有读取到图像')
+                return None
+            data_encode = np.array(img_encode)								    # 将编码数据转换成二进制数据
+            self.stream.write(data_encode)										# 将二进制数据存放到io流
+            self.connect.write(struct.pack('<L', self.stream.tell()))			# struct.pack()将数据转换成什么格式    stream.tell()获得目前指针的位置，将数据写入io流后，数据指针跟着后移，
+                                                                                # 也就是将数据长度转换成'<L'类型（无符号长整型），写入makefile传输文件
+                                                                                # 它的作用相当于 帧头数据，单独收到这个数据表示开始传输一帧图片数据，因为图片大小确定，这个数也就定下不变
+            self.connect.flush()											    # 刷新，将数据长度发送出去
+            self.stream.seek(0)													# 更新io流，将指针指向0
+            self.connect.write(self.stream.read())								# 指针指向0后，从头开始读数据，然后写到makefile传输文件
+            self.stream.seek(0)													# 更新指针
+            self.stream.truncate()												# 更新io流数据，删除指针后面的数据
 
-                self.connect.write(struct.pack('<L', 0))						    # 发送0，相当于帧尾数据，单独收到这个数表示一帧图片传输结束
-            except ConnectionResetError:
-                print('连接已重置')
-                self.connecting()
+            self.connect.write(struct.pack('<L', 0))						    # 发送0，相当于帧尾数据，单独收到这个数表示一帧图片传输结束
+        except ConnectionResetError:
+            # print('连接已重置')
+            self.connecting()
 
 
 if __name__ == '__main__':
-    stream = VideoStreaming('10.0.0.2', 8000)
+    stream = VideoStreaming('10.0.0.3', 8000)
     cap = cv2.VideoCapture(0)
     stream.connecting()
     stream.start()
     while True:
         img = cap.read()[1]
         stream.send(img)
-        if cv2.waitKey(1) == 27:
-            break
